76cee1fa5a36cd5823d9e7c1d81dd09a45a9b40f
Stanislav Nikolov <hello@snikolov.me>
2026-05-12 13:39:44 +0300

    update models:
    - fix Category label in the Admin
    - update `related_name`s
    -add DB indices to the searchable fields
    - make the image optional, so that it can be set/unset separately

--- backend/ecommapp/models.py
+++ backend/ecommapp/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "categories"
+
     name = models.CharField(max_length=50)
     parent = models.ForeignKey(
         'self',
         on_delete=models.PROTECT,
         null=True,
         blank=True,
-        related_name='products',
+        related_name="subcategories",
     )
 
     def __str__(self):
@@ -15,24 +18,21 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    sku = models.CharField(
-        max_length=50,
-        unique=True,
-        db_index=True,
-        primary_key=True,
-    )
-    title = models.CharField(max_length=200)
+    sku = models.CharField(max_length=50, unique=True, db_index=True)
+    title = models.CharField(max_length=200, db_index=True)
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(blank=True)
     price = models.DecimalField(
         max_digits=10, 
         decimal_places=2,
+        db_index=True,
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.PROTECT,
+        related_name='products',
+        db_index=True,
     )
 
     def __str__(self):
         return f"{self.title} ({self.sku})"
-
