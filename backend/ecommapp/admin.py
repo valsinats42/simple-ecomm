8b118afd7ab6d842100bd68217d9be8242bab0c7
Stanislav Nikolov <hello@snikolov.me>
2026-05-12 14:34:02 +0300

    update admin interface to show all relevant fields

--- backend/ecommapp/admin.py
+++ backend/ecommapp/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image_tag', 'sku', 'title', 'price', 'category')
+    list_display_links = ('image_tag', 'sku', 'title',)
+
+    @admin.display(description="Image")
+    def image_tag(self, obj):
+        return format_html(
+            '<img src="{}" width="50" height="50" style="object-fit: contain;" />',
+            obj.image.url if obj.image else '#')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'parent')
+    list_display_links = ('id', 'name')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- backend/simpleecomm/urls.py
+++ backend/simpleecomm/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("app/", include("ecommapp.urls")),
     path("admin/", admin.site.urls),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
